7223e27f2910ae6d148eba731aeef765010c069c
Tristan <deaugustinetristan@gmail.com>
2019-08-11 16:39:35 -0400

    its alive

--- public/main.js
+++ public/main.js
@@ -1,7 +1,218 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let textArea = document.getElementById('text-area')
+let newGameButton = document.getElementById('new-game-button')
+let hitButton = document.getElementById('hit-button')
+let stayButton = document.getElementById('stay-button')
+
+hitButton.style.display = 'none'
+stayButton.style.display = 'none'
+
+let gameStart = false,
+  gameOver = false,
+  playWon = false,
+  dealerCards = [],
+  playerCards = [],
+  dealerScore = 0,
+  playerScore = 0,
+  playerStays = false,
+  deck = []
+
+newGameButton.addEventListener('click', function() {
+  gameStarted = true
+  gameOver = false
+  playerWon = false
+  playerStays = false
+
+  deck = createDeck()
+  deck = shuffleDeck(deck)
+  dealerCards = [getNextCard(), getNextCard()]
+  playerCards = [getNextCard(), getNextCard()]
+  newGameButton.style.display = 'none'
+  hitButton.style.display = 'inline'
+  stayButton.style.display = 'inline'
+  showStatus()
+})
+
+function createDeck() {
+  deckOfCards = [
+    [1, 'Clubs', 'Ten', 10, '10_of_clubs.svg'],
+    [2, 'Diamonds', 'Ten', 10, '10_of_diamonds.svg'],
+    [3, 'Hearts', 'Ten', 10, '10_of_hearts.svg'],
+    [4, 'Spades', 'Ten', 10, '10_of_spades.svg'],
+    [5, 'Clubs', 'Two', 2, '2_of_clubs.svg'],
+    [6, 'Diamonds', 'Two', 2, '2_of_diamonds.svg'],
+    [7, 'Hearts', 'Two', 2, '2_of_hearts.svg'],
+    [8, 'Spades', 'Two', 2, '2_of_spades.svg'],
+    [9, 'Clubs', 'Three', 3, '3_of_clubs.svg'],
+    [10, 'Diamonds', 'Three', 3, '3_of_diamonds.svg'],
+    [11, 'Hearts', 'Three', 3, '3_of_hearts.svg'],
+    [12, 'Spades', 'Three', 3, '3_of_spades.svg'],
+    [13, 'Clubs', 'Four', 4, '4_of_clubs.svg'],
+    [14, 'Diamonds', 'Four', 4, '4_of_diamonds.svg'],
+    [15, 'Hearts', 'Four', 4, '4_of_hearts.svg'],
+    [16, 'Spades', 'Four', 4, '4_of_spades.svg'],
+    [17, 'Clubs', 'Five', 5, '5_of_clubs.svg'],
+    [18, 'Diamonds', 'Five', 5, '5_of_diamonds.svg'],
+    [19, 'Hearts', 'Five', 5, '5_of_hearts.svg'],
+    [20, 'Spades', 'Five', 5, '5_of_spades.svg'],
+    [21, 'Clubs', 'Six', 6, '6_of_clubs.svg'],
+    [22, 'Diamonds', 'Six', 6, '6_of_diamonds.svg'],
+    [23, 'Hearts', 'Six', 6, '6_of_hearts.svg'],
+    [24, 'Spades', 'Six', 6, '6_of_spades.svg'],
+    [25, 'Clubs', 'Seven', 7, '7_of_clubs.svg'],
+    [26, 'Diamonds', 'Seven', 7, '7_of_diamonds.svg'],
+    [27, 'Hearts', 'Seven', 7, '7_of_hearts.svg'],
+    [28, 'Spades', 'Seven', 7, '7_of_spades.svg'],
+    [29, 'Clubs', 'Eight', 8, '8_of_clubs.svg'],
+    [30, 'Diamonds', 'Eight', 8, '8_of_diamonds.svg'],
+    [31, 'Hearts', 'Eight', 8, '8_of_hearts.svg'],
+    [32, 'Spades', 'Eight', 8, '8_of_spades.svg'],
+    [33, 'Clubs', 'Nine', 9, '9_of_clubs.svg'],
+    [34, 'Diamonds', 'Nine', 9, '9_of_diamonds.svg'],
+    [35, 'Hearts', 'Nine', 9, '9_of_hearts.svg'],
+    [36, 'Spades', 'Nine', 9, '9_of_spades.svg'],
+    [37, 'Clubs', 'Ace', 11, 'ace_of_clubs.svg'],
+    [38, 'Diamonds', 'Ace', 11, 'ace_of_diamonds.svg'],
+    [39, 'Hearts', 'Ace', 11, 'ace_of_hearts.svg'],
+    [40, 'Spades', 'Ace', 11, 'ace_of_spades.svg'],
+    [41, 'Clubs', 'Jack', 10, 'jack_of_clubs.svg'],
+    [42, 'Diamonds', 'Jack', 10, 'jack_of_diamonds.svg'],
+    [43, 'Hearts', 'Jack', 10, 'jack_of_hearts.svg'],
+    [44, 'Spades', 'Jack', 10, 'jack_of_spades.svg'],
+    [45, 'Clubs', 'King', 10, 'king_of_clubs.svg'],
+    [46, 'Diamonds', 'King', 10, 'king_of_diamonds.svg'],
+    [47, 'Hearts', 'King', 10, 'king_of_hearts.svg'],
+    [48, 'Spades', 'King', 10, 'king_of_spades.svg'],
+    [49, 'Clubs', 'Queen', 10, 'queen_of_clubs.svg'],
+    [50, 'Diamonds', 'Queen', 10, 'queen_of_diamonds.svg'],
+    [51, 'Hearts', 'Queen', 10, 'queen_of_hearts.svg'],
+    [52, 'Spades', 'Queen', 10, 'queen_of_spades.svg']
+  ]
+
+  return deckOfCards
+}
+function shuffleDeck(deck) {
+  let myDeck = []
+  for (i = 0; i < 52; i++) {
+    // empty array
+    let random = Math.floor(Math.random() * (deck.length - 0)) + 0
+    //console.log(random,deckOfCards.length);
+    tmpArray = deck[random]
+    myDeck.push(tmpArray)
+    deck.splice(random, 1)
+  }
+  return myDeck
+}
+
+hitButton.addEventListener('click', function() {
+  playerCards.push(getNextCard())
+  checkForEndOfGame()
+  showStatus()
+})
+
+stayButton.addEventListener('click', function() {
+  alert(dealerScore)
+  while (dealerScore < 18) {
+    dealerCards.push(getNextCard())
+    alert(dealerScore)
+    checkForEndOfGame()
+    showStatus()
+  }
+})
+
+function checkForEndOfGame() {
+  updateScores()
+
+  if (gameOver) {
+    while (
+      dealerScore < playerScore &&
+      playerScore <= 21 &&
+      dealerScore <= 21
+    ) {
+      //dealerScore.push(getNextCard())
+      updateScores()
+    }
+  }
+
+  if (playerScore > 21) {
+    playerWon = false
+    gameOver = true
+  } else if (dealerScore > 21) {
+    playerWon = true
+    gameOver = true
+  } else if (gameOver) {
+    if (playerScore > dealerScore) {
+      playerWon = true
+    } else {
+      playerWon = false
+    }
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+function showStatus() {
+  if (!gameStarted) {
+    textArea.innerText = 'Welcome to Blackjack!'
+    return
+  }
+
+  let dealerCardString = ''
+  for (let i = 0; i < dealerCards.length; i++) {
+    dealerCardString += dealerCards[i][2] + ' of ' + dealerCards[i][1] + '\n'
+  }
+  let playerCardString = ''
+  for (let i = 0; i < playerCards.length; i++) {
+    playerCardString += playerCards[i][2] + ' of ' + playerCards[i][1] + '\n'
+  }
+
+  updateScores()
+
+  /*textArea.innerText =
+    'Dealer has:\n' +
+    dealerCardString +
+    '(score: ' +
+    dealerScore +        
+    ')\n\n' +
+     */
+  textArea.innerText =
+    'Player has:\n' + playerCardString + '(score: ' + playerScore + ')\n\n'
+
+  if (gameOver) {
+    if (playerWon) {
+      textArea.innerText += 'YOU WIN!'
+    } else {
+      textArea.innerText += 'DEALER WINS'
+    }
+    newGameButton.style.display = 'inline'
+    hitButton.style.display = 'none'
+    stayButton.style.display = 'none'
+  }
+}
+
+function getScore(cardArray) {
+  let score = 0
+  let hasAce = false
+  for (let i = 0; i < cardArray.length; i++) {
+    score += cardArray[i][3]
+    if (cardArray[i][2]) 
+  }
+  return score
+}
+
+function updateScores() {
+  dealerScore = getScore(dealerCards)
+  playerScore = getScore(playerCards)
+}
+
+function getNextCard() {
+  return deck.shift()
+}
+
+function checkScores() {
+  updateScores()
+  if (playerScore == 21)
+  {
+    gameOver = true
+    showStatus()
+
+  }
+
+}
